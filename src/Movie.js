441384c5dae8192de1f7b2248bacb4551837fa93
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2023-05-24 15:47:47 -0700

    deleted unused comments

--- src/Movie.js
+++ src/Movie.js
@@ -43,19 +43,3 @@ function Movie({ movie, favorites, addToFavorites, removeFromFavorites }) {
 }
 
 export default Movie;
-
-// function cartIcon() {
-// 	const alreadyInCart = cartItems.some((item) => item.id === img.id);
-// 	if (alreadyInCart) {
-// 		return (
-// 			<i
-// 				className="ri-shopping-cart-fill cart"
-// 				onClick={() => removeFromCart(img.id)}
-// 			></i>
-// 		);
-// 	} else if (hovered) {
-// 		return (
-// 			<i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
-// 		);
-// 	}
-// }
